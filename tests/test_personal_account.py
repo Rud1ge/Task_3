e85227ba8c261a90a8dceaa2236ae669f93882e5
Руди Герман Геннадьевич <german.rudi@eltex.loc>
2026-07-17 17:23:20 +0700

    test(personal-account): add ui tests for personal account

---
+++ tests/test_personal_account.py
@@ -0,0 +1,44 @@
+import allure
+
+from pages.account_page import AccountPage
+from pages.login_page import LoginPage
+from urls import ACCOUNT_ORDERS_URL, ACCOUNT_URL, BASE_URL, LOGIN_URL
+
+
+@allure.link(BASE_URL, name="Stellar Burgers")
+class TestPersonalAccount:
+    @allure.title("Переход в личный кабинет")
+    @allure.description("После авторизации клик по «Личный кабинет» открывает /account.")
+    def test_navigate_to_account(self, driver, user):
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.login(user["email"], user["password"])
+        login_page.go_to_personal_account()
+
+        assert ACCOUNT_URL in driver.current_url
+
+    @allure.title("Переход в историю заказов")
+    @allure.description("Из личного кабинета можно перейти в раздел «История заказов».")
+    def test_navigate_to_order_history(self, driver, user):
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.login(user["email"], user["password"])
+        login_page.go_to_personal_account()
+
+        account_page = AccountPage(driver)
+        account_page.go_to_order_history()
+
+        assert ACCOUNT_ORDERS_URL in driver.current_url
+
+    @allure.title("Выход из аккаунта")
+    @allure.description("После выхода пользователь перенаправляется на страницу входа.")
+    def test_logout(self, driver, user):
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.login(user["email"], user["password"])
+        login_page.go_to_personal_account()
+
+        account_page = AccountPage(driver)
+        account_page.logout()
+
+        assert LOGIN_URL in driver.current_url
