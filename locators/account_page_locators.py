277524be9fbc7a8f13e3dda64d2d374cff09760d
Руди Герман Геннадьевич <german.rudi@eltex.loc>
2026-07-17 17:59:43 +0700

    fix(locators): align selectors with current app routes
    Update order history URL, password field locators and order number matching for the current UI.

--- locators/account_page_locators.py
+++ locators/account_page_locators.py
@@ -1,13 +1,10 @@
 from selenium.webdriver.common.by import By
 
-ORDER_HISTORY_LINK = (By.XPATH, ".//a[contains(@href, '/account/orders')]")
+ORDER_HISTORY_LINK = (By.XPATH, ".//a[contains(@href, '/account/order-history')]")
 LOGOUT_BUTTON = (By.XPATH, ".//button[contains(., 'Выход')]")
-ORDER_HISTORY_LINKS = (By.XPATH, ".//a[contains(@href, '/account/orders/')]")
+ORDER_HISTORY_ITEMS = (By.XPATH, ".//li[contains(@class, 'OrderHistory') or contains(@class, 'order')]")
 
 
 def order_number_in_history(order_number):
     formatted_number = f"{order_number:07d}"
-    return (
-        By.XPATH,
-        f".//a[contains(@href, '/account/orders/') and contains(., '#{formatted_number}')]",
-    )
+    return (By.XPATH, f".//*[contains(text(), '#{formatted_number}')]")
--- locators/reset_password_page_locators.py
+++ locators/reset_password_page_locators.py
@@ -1,9 +1,9 @@
 from selenium.webdriver.common.by import By
 
 PAGE_TITLE = (By.XPATH, ".//h2[contains(text(), 'Восстановление пароля')]")
-PASSWORD_INPUT = (By.XPATH, ".//input[@type='password' or @name='password']")
+PASSWORD_INPUT = (By.XPATH, ".//input[@name='Введите новый пароль']")
 PASSWORD_VISIBILITY_TOGGLE = (
     By.XPATH,
-    ".//input[@type='password' or @name='password']"
+    ".//input[@name='Введите новый пароль']"
     "/ancestor::div[contains(@class, 'input')]//div[contains(@class, 'icon')]",
 )
