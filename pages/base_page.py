48dda51a7f7898709dfe9b15ce9f39180c554459
Руди Герман Геннадьевич <german.rudi@eltex.loc>
2026-07-20 10:44:11 +0700

    fix(pages): wait for account URL after personal cabinet click

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,4 @@
 import allure
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -13,24 +12,16 @@ class BasePage:
         self.driver = driver
 
     def wait_for_visibility(self, locator, timeout=DEFAULT_TIMEOUT):
-        return WebDriverWait(self.driver, timeout).until(
-            expected_conditions.visibility_of_element_located(locator)
-        )
+        return WebDriverWait(self.driver, timeout).until(expected_conditions.visibility_of_element_located(locator))
 
     def wait_for_clickable(self, locator, timeout=DEFAULT_TIMEOUT):
-        return WebDriverWait(self.driver, timeout).until(
-            expected_conditions.element_to_be_clickable(locator)
-        )
+        return WebDriverWait(self.driver, timeout).until(expected_conditions.element_to_be_clickable(locator))
 
     def wait_for_url_contains(self, url_part, timeout=DEFAULT_TIMEOUT):
-        return WebDriverWait(self.driver, timeout).until(
-            expected_conditions.url_contains(url_part)
-        )
+        return WebDriverWait(self.driver, timeout).until(expected_conditions.url_contains(url_part))
 
     def wait_until_invisible(self, locator, timeout=DEFAULT_TIMEOUT):
-        return WebDriverWait(self.driver, timeout).until(
-            expected_conditions.invisibility_of_element_located(locator)
-        )
+        return WebDriverWait(self.driver, timeout).until(expected_conditions.invisibility_of_element_located(locator))
 
     def find_elements(self, locator):
         return self.driver.find_elements(*locator)
@@ -84,3 +75,4 @@ class BasePage:
     @allure.step("Переходим в личный кабинет")
     def go_to_personal_account(self):
         self.click(header_locators.PERSONAL_ACCOUNT_LINK)
+        self.wait_for_url_contains("/account")
